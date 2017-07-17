c46746809195b5519bd213905420d9e7605f2b52
Hiroaki Egashira <ehiroaki5@gmail.com>
2017-07-17 20:51:14 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Hello world")
+
+	// _, err := ioutil.ReadFile(args.FileName)
+	// if err != nil {
+	// 	fmt.Println("failed to read the specified file:", args.FileName)
+	// 	return
+	// }
+}
